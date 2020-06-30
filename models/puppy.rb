18da0e6c096d4a0f16ad825aeeb7b4b18a815a26
Luca <lucakanyo@protonmail.com>
2020-06-30 10:25:47 -0400

    Done.

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,1 +1,16 @@
-# since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
+class Puppy 
+    attr_accessor :name, :breed
+    attr_reader :age
+
+
+    def initialize(name, breed, age)
+        @name = name 
+        @age = age 
+        @breed = breed
+    end 
+
+    def age=(age) 
+        @age = age
+        @age * 12
+    end 
+end 
