e2babf7f75b945b235e258698a66b5592e3885cc
ejguzman3988 <eriberto176@gmail.com>
2020-07-10 03:55:47 -0400

    Done.

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,1 +1,13 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
+class Puppy
+    attr_accessor :name, :breed, :age
+    @@all = []
+    def initialize(attributes)
+        attributes.each{|key,value| send("#{key}=",value)}
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
